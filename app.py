4e36fbe70eacff4ad069b5e54c9671918cdd0851
Vaishnavikavi <54409558+Vaishnavikavi@users.noreply.github.com>
2026-07-30 23:49:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,37 +2,24 @@ import os
 import requests
 
 from fastapi import FastAPI, Request
-from google import genai
+
+from agent import process_message
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
-client = genai.Client(api_key=GEMINI_API_KEY)
-
-MODEL = "gemini-flash-latest"
 
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    models = []
-
-    try:
-        for model in client.models.list():
-            models.append({
-                "name": model.name,
-                "methods": getattr(model, "supported_generation_methods", [])
-            })
-
-        return models
-
-    except Exception as e:
-        return {"error": str(e)}
+    return {
+        "status": "running"
+    }
 
 
 @app.post("/webhook")
 async def webhook(request: Request):
+
     update = await request.json()
 
     message = update.get("message")
@@ -41,9 +28,13 @@ async def webhook(request: Request):
         return {"ok": True}
 
     chat_id = message["chat"]["id"]
+
     text = message.get("text", "")
 
-    answer = ask_llm(text)
+    answer = process_message(
+        chat_id,
+        text
+    )
 
     send_message(chat_id, answer)
 
@@ -51,29 +42,14 @@ async def webhook(request: Request):
 
 
 def send_message(chat_id, text):
+
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    response = requests.post(
+    requests.post(
         url,
         json={
             "chat_id": chat_id,
-            "text": text,
+            "text": text
         },
-        timeout=30,
+        timeout=60
     )
-
-    print("Telegram Status:", response.status_code)
-    print("Telegram Response:", response.text)
-
-
-def ask_llm(user_message: str):
-    try:
-        response = client.models.generate_content(
-            model=MODEL,
-            contents=user_message,
-        )
-        return response.text
-
-    except Exception as e:
-        print(e)
-        return f"Error: {e}"
