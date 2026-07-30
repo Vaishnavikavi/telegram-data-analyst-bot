f6b24a30487851486160dd60344e985ef517b079
Vaishnavikavi <54409558+Vaishnavikavi@users.noreply.github.com>
2026-07-30 22:54:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
-from fastapi import FastAPI
+import os
+import requests
+
+from fastapi import FastAPI, Request
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {"status": "running"}
+
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    update = await request.json()
+
+    message = update.get("message")
+
+    if not message:
+        return {"ok": True}
+
+    chat_id = message["chat"]["id"]
+    text = message.get("text", "")
+
+    send_message(chat_id, f"You said:\n{text}")
+
+    return {"ok": True}
+
+
+def send_message(chat_id, text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(
+        url,
+        json={
+            "chat_id": chat_id,
+            "text": text,
+        },
+        timeout=30,
+    )
